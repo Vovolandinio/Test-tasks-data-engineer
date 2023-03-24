f3577ebfbe1771a25fda2c0a7b9e8159e4b0882c
Vovolandinio <nanowatcher65@gmail.com>
2023-03-24 20:28:13 +0300

    change schema for task 2

--- task2.sql
+++ task2.sql
@@ -1,13 +1,15 @@
+CREATE SCHEMA task_two
+
 -- Задание: нужно для каждого дня определить последнее местоположение абонента.
 
-CREATE TABLE test_task.calls
+CREATE TABLE task_two.calls
 (
     abonent   varchar(10),
     region_id varchar(5),
     dttm      timestamp
 );
 
-INSERT INTO test_task.calls (abonent, region_id, dttm)
+INSERT INTO task_two.calls (abonent, region_id, dttm)
 VALUES ('7072110988', '32722', '2021-08-18 13:15'),
        ('7072110988', '32722', '2021-08-18 14:00'),
        ('7072110988', '21534', '2021-08-18 14:15'),
@@ -15,8 +17,8 @@ VALUES ('7072110988', '32722', '2021-08-18 13:15'),
        ('7071107101', '12533', '2021-08-19 09:15'),
        ('7071107101', '32722', '2021-08-19 09:27');
 
-SELECT DISTINCT ON (date_trunc('day', test_task.calls.dttm), test_task.calls.abonent) test_task.calls.abonent,
-                                                                                      test_task.calls.region_id,
-                                                                                      test_task.calls.dttm
-FROM test_task.calls
-ORDER BY test_task.calls.abonent DESC, date_trunc('day', test_task.calls.dttm), test_task.calls.dttm DESC;
+SELECT DISTINCT ON (date_trunc('day', task_two.calls.dttm), task_two.calls.abonent) task_two.calls.abonent,
+                                                                                      task_two.calls.region_id,
+                                                                                      task_two.calls.dttm
+FROM task_two.calls
+ORDER BY task_two.calls.abonent DESC, date_trunc('day', task_two.calls.dttm), task_two.calls.dttm DESC;
